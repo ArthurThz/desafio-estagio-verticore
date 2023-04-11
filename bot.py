a1410128017602a096c7924db4b4a375bc7f460f
Arthur Theodoro | Front End Developer <arthur.theodoro.sva@hotmail.com>
2023-04-11 02:15:10 -0300

    funcoes de coleta de dados implementadas, criacao da tabela excel tambem

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 
 
-
+import openpyxl
 # Import for the Desktop Bot
 from botcity.core import DesktopBot
 
@@ -20,8 +20,8 @@ def main():
     ## Fetch the BotExecution with details from the task, including parameters
     execution = maestro.get_execution()
 
-    print(f"Task ID is: {execution.task_id}")
-    print(f"Task Parameters are: {execution.parameters}")
+    # print(f"Task ID is: {execution.task_id}")
+    # print(f"Task Parameters are: {execution.parameters}")
 
     desktop_bot = DesktopBot()
 
@@ -31,78 +31,94 @@ def main():
     webbot.headless = False
 
    
-   
     webbot.driver_path = "chromedriver_win32/chromedriver.exe"
 
-    # Opens the BotCity website.
-    webbot.browse("https://cidades.ibge.gov.br/")
+    #Criação da planilha
+
+    book = openpyxl.Workbook()
+
+    Page_Informacoes = book["Sheet"]
+
+    Page_Informacoes.append(["Gentilico","Capital","Governador","População","IDH"])
+    book.save("dados_ibge.xlsx")
+    
+    dados_coletados = []   
+
+    def getGentilico():
+        
+        if not webbot.find_text( "gentilico", matching=0.97, waiting_time=10000):
+            not_found("gentilico")              
+        webbot.triple_click_relative(1,27)
+        webbot.control_c()
+        gentilico = webbot.get_clipboard()
+        dados_coletados.append(gentilico)
+
+    def getCapital():
+        if not webbot.find_text( "find_capital", matching=0.97, waiting_time=10000):
+            not_found("find_capital")
+        webbot.triple_click_relative(1, 20)
+        webbot.control_c()
+        capital = webbot.get_clipboard()
+        dados_coletados.append(capital)
 
     
-    webbot.wait(1000)
+    def getGovernador():
+        if not webbot.find_text( "find_governador", matching=0.97, waiting_time=10000):
+           not_found("find_governador")
+        webbot.triple_click_relative(1, 20)
+        webbot.control_c()
+        governador = webbot.get_clipboard()
+        dados_coletados.append(governador)       
+
+    def getPopulacao():
+       if not webbot.find_text( "find_populacao", matching=0.97, waiting_time=10000):
+           not_found("find_populacao")
+       webbot.double_click_relative(124, 109)
+       webbot.control_c()
+       populacao = webbot.get_clipboard()
+       dados_coletados.append(populacao)
+
+    def getIDH():
+        if not webbot.find_text( "open_economia", matching=0.97, waiting_time=10000):
+            not_found("open_economia")
+        webbot.click()
+        if not webbot.find_text( "find_idh", matching=0.97, waiting_time=10000):
+            not_found("find_idh")
+        webbot.triple_click_relative(277, 45)
+        webbot.control_c()
+        idh = webbot.get_clipboard()
+        dados_coletados.append(idh)
+        
+        
+        
+
    
-    if not webbot.find( "comece", matching=0.97, waiting_time=10000):
-        not_found("comece")
-    webbot.click()
-    
-    if not webbot.find( "find_estados", matching=0.97, waiting_time=10000):
-        not_found("find_estados")
-    webbot.click()
+    def searchData(sigla):
+        
+        webbot.browse(f"https://cidades.ibge.gov.br/brasil/{sigla}/panorama")
+        webbot.maximize_window()
+        webbot.wait(1000)
+        getGentilico()
+        getCapital()
+        getGovernador()
+        getPopulacao()
+        getIDH()     
+        print(dados_coletados)
+        Page_Informacoes.append(dados_coletados)
+        book.save("dados_ibge.xlsx")
 
-    webbot.wait(1000)
-    
-    if not webbot.find( "acre", matching=0.97, waiting_time=10000):
-        not_found("acre")
-    webbot.click()
 
-    webbot.wait(1000)
-    
-    if not webbot.find( "find_gentilico", matching=0.97, waiting_time=10000):
-        not_found("find_gentilico")   
-    
-    webbot.double_click_relative(25, 23)
-    webbot.control_c()    
+    searchData("sp")
+           
 
-    gentilico = webbot.get_clipboard()
-    print(gentilico)
-    webbot.wait(1000)
-
-    if not webbot.find( "find_capital", matching=0.97, waiting_time=10000):
-        not_found("find_capital")
-
-    webbot.triple_click_relative(25, 23)
-    webbot.control_c() 
-    capital =  webbot.get_clipboard()      
-    print(capital)
-
-    webbot.wait(1000)
+    # estados = ["sp","ac","pe"]  
     
-    if not webbot.find( "find_governador", matching=0.97, waiting_time=10000):
-        not_found("find_governador")    
-    
-    webbot.triple_click_relative(25, 23)
-    webbot.control_c() 
-    governador =  webbot.get_clipboard()      
-    print(governador)
-    webbot.wait(2000)
-
-    if not webbot.find( "find_populacao", matching=0.97, waiting_time=10000):
-        not_found("find_populacao")
-    
-    webbot.double_click_relative(275, 6)
-       
-    
-    
-    
-    
-    
-    
-
-  
-   
-
- 
-
-    # Uncomment to mark this task as finished on BotMaestro
+    # for sigla in estados:
+    #     webbot.browse(f"https://cidades.ibge.gov.br/brasil/{sigla}/panorama")
+    #     webbot.maximize_window()
+    #     searchData()
+        
+     # Uncomment to mark this task as finished on BotMaestro
     # maestro.finish_task(
     #     task_id=execution.task_id,
     #     status=AutomationTaskFinishStatus.SUCCESS,
@@ -115,4 +131,10 @@ def not_found(label):
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
 
